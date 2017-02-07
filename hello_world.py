c446a64e63d49162ac91401fd0a135ee7d3d1884
Kiren Jacob <kirenjacobthomas@gmail.com>
2017-02-08 00:41:50 +0530

    Removed hello_world.py

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-import time;
-
-print 'hello world!'
-myName = 'Kiren'
-myAge = 23
-print "My name is %s and I'm %d years old!" % (myName, myAge);
-time_tuple = time.localtime(time.time());
-date = time_tuple
-print "The time right now is ", time.asctime(time_tuple); 
